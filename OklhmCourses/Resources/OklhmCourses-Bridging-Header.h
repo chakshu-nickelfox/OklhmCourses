6b853d9facb42beb9293de8e52484f28b66d81dd
chakshu-nickelfox <chakshu@nickelfox.com>
2024-05-24 16:53:20 +0530

    initial commit

---
+++ OklhmCourses/Resources/OklhmCourses-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  OklhmCourses-Bridging-Header.h
+//  OklhmCourses
+//
+//  Created by Ravindra Soni on 30/11/18.
+//  Copyright © 2018 Nickelfox. All rights reserved.
+//
+
+#ifndef OklhmCourses_Bridging_Header_h
+#define OklhmCourses_Bridging_Header_h
+
+
+#endif /* OklhmCourses_Bridging_Header_h */
---
+++ Pods/Target Support Files/Pods-OklhmCourses/Pods-OklhmCourses-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_OklhmCourses : NSObject
+@end
+@implementation PodsDummy_Pods_OklhmCourses
+@end
